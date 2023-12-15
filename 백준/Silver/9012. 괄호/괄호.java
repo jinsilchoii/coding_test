50d01422b42a86863135af785bcbfbca19a86ca5
jinsilchoii <153526456+jinsilchoii@users.noreply.github.com>
2023-12-15 16:47:33 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-
-        int n = Integer.parseInt(sc.next());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for (int i=0; i<n; i++) {
-            String input = sc.next();
+            String input = br.readLine();
             String res = "YES";
 
             Stack st = new Stack();
@@ -18,7 +19,7 @@ public class Main {
                 if (input.charAt(j) == '(') {
                     st.push(1);
                 } else if (input.charAt(j) == ')') {
-                    if(st.isEmpty()) {
+                    if (st.isEmpty()) {
                         res = "NO";
                         break;
                     } else {
@@ -27,7 +28,7 @@ public class Main {
                 }
             }
 
-            if(!st.isEmpty()) {
+            if (!st.isEmpty()) {
                 res = "NO";
             }
             System.out.println(res);
