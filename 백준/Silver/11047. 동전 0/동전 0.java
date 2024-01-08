504f6de24c61001bfec923036a00ebfa4181e2f7
jinsilchoii <153526456+jinsilchoii@users.noreply.github.com>
2024-01-08 17:48:28 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,12 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
@@ -26,6 +26,8 @@ public class Main {
             }
         }
 
-        System.out.println(cnt);
+        bw.write(String.valueOf(cnt));
+        br.close();
+        bw.close();
     }
 }
